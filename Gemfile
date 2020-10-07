e2fbd8fdabcb823fffa1700448db79997e82b1a2
Sediqwe <sediqwe@gmail.com>
2020-10-07 22:39:17 +0200

    beindulunk

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'less-rails'
+gem 'semantic-ui-sass'
+gem 'jquery-rails'
+gem 'therubyracer'
 
 group :production do 
   gem 'pg'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Loads all Semantic javascripts
+//= require semantic-ui
+//= require jquery
+require("semantic-ui-sass")
+$(document).on('turbolinks:load', function(){
+    $('.ui.dropdown').dropdown();
+})
