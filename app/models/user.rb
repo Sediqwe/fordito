6497052873628fbb25823a85aec9d8989eb98e7f
Sediqwe <sediqwe@gmail.com>
2020-10-26 22:29:22 +0100

    Regisztráció

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-    has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_one_attached :avatar
+    validates :avatar, presence:true
+    validates :email, uniqueness: true
+    validates :username, length: {minimum:4, maximum:20},uniqueness: true
+    
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :uploads
   resources :users
   resources :projects
