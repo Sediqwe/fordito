2b03e12e7c8e31fc434abc0e35c544ab105cf3e8
Sediqwe <sediqwe@gmail.com>
2020-10-27 15:15:39 +0100

    próbacseresznye

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+  self.table_name = "users"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     has_one_attached :avatar
